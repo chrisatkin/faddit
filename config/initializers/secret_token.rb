50f6fe4884b863d8985b14dfc9380bfc9140ab55
Chris Atkin <me@chrisatk.in>
2014-01-31 14:47:54 -0800

    Install Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Faddit::Application.config.secret_key_base = '8fbabdf7b5cb3b6890c6195fddfa2a5ad5545a0870cadf93d60117f51205878efe78e094def898da4b11388ac3c71bf617d50705679c4290eee6384e0d2042ec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Faddit::Application.config.session_store :cookie_store, key: '_faddit_session'
